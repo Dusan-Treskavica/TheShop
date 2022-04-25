6f88941c70b1af5f1fab750d3329581a1245ff92
Dusan Treskavica <d.treskavica@sap.com>
2022-04-25 15:55:01 +0200

    Created separated files for every class and renamed some properties in Article

---
+++ TheShop/Model/Supplier2.cs
@@ -0,0 +1,20 @@
+﻿namespace TheShop.Model
+{
+    public class Supplier2
+	{
+		public bool ArticleInInventory(int id)
+		{
+			return true;
+		}
+
+		public Article GetArticle(int id)
+		{
+			return new Article()
+			{
+				Id = 1,
+				ArticleName = "Article from supplier2",
+				ArticlePrice = 459
+			};
+		}
+	}
+}
